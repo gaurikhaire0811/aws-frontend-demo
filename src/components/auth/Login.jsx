2d51860e2f65968f6f486cef020a197e7e623edf
gaurikhaire0811 <gaurikhaire191@gmail.com>
2026-08-09 13:32:43 +0530

    Update login API endpoint to new server address

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
 
     try {
       setLoading(true);
-      const res = await axios.post("http://localhost:3002/login", {
+      const res = await axios.post("http://16.170.8.203:3002/login", {
         email: email,
         password: password,
       });
